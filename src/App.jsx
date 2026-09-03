539af699278dca511e8030ebd38095fff3ffc3cc
Jakly Medalla <medallacreate@gmail.com>
2026-09-03 10:53:08 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  const [theme, setTheme] = ('light')
+
+  return (
+    <div className='dark:bg-black relative'>
+      <Navbar theme={theme} setTheme={setTheme}/>
+    </div>
+  )
+}
+
+export default App
