e86ab74512a32461a3a41c1087ccf91148d300d3
Jakly Medalla <medallacreate@gmail.com>
2026-09-11 10:59:18 +0800

    progress

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import assets from "../assets/assets";
+import { motion } from "motion/react";
 
 const Hero = () => {
   return (
@@ -8,14 +9,22 @@ const Hero = () => {
       className="flex flex-col items-center gap-6 py-20 px-4 sm:px-12 lg:px-24
     xl:px-40 text-center w-full overflow-hidden text-gray-700 dark:text:white"
     >
-      <div
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.7 }}
+        viewport={{ once: true }}
         className="inline-flex items-center gap-2 border border-gray-300 p-1.5
     pr-4 rounded-full"
       >
         <img className="w-20" src={assets.group_profile} alt="group-photo" />
         <p className="text-xs font-medium">Trusted by 10k+ people</p>
-      </div>
-      <h1
+      </motion.div>
+      <motion.h1
+        initial={{ opacity: 0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.8 }}
+        viewport={{ once: true }}
         className="text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium
     xl:leading-[95px] max-w-5xl"
       >
@@ -27,15 +36,27 @@ const Hero = () => {
           digital
         </span>{" "}
         impact.
-      </h1>
-      <p
+      </motion.h1>
+
+      <motion.p
+        initial={{ opacity: 0, y: 30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 1 }}
+        viewport={{ once: true }}
         className="text-sm sm:text-lg font-medium text-gray-500 dark:text-white/75
     max-w-4/5 sm:max-w-lg pb-3"
       >
         Creating meaningful connections and turning big ideas into interactive
         digital experiences
-      </p>
-      <div className="relative">
+      </motion.p>
+
+      <motion.div
+        initial={{ opacity: 0, scale: 0.9 }}
+        whileInView={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.6, delay: 2 }}
+        viewport={{ once: true }}
+        className="relative"
+      >
         <img src={assets.hero_img} alt="hero" className="w-full max-w-6xl" />
         <img
           src={assets.bgImage1}
@@ -43,7 +64,7 @@ const Hero = () => {
           className="absolute -top-40 sm:-top-100 left-1/2 -translate-x-1/2 -z-1 
       dark:hidden object-contain"
         />
-      </div>
+      </motion.div>
     </div>
   );
 };
